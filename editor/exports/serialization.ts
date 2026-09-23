2d1e8dcb4380c45bf832bd55d17bebe03040e6eb
AlienHead <mad.alexis@live.com>
2026-09-23 12:42:41 +0300

    feat(cage): expose cage bake helpers to editor extensions

--- editor/exports/serialization.ts
+++ editor/exports/serialization.ts
@@ -9,3 +9,11 @@ export {
 export {
     serializeBuiltinValueType,
 } from '../../cocos/serialization/compiled/builtin-value-type';
+
+
+export {
+    buildDefaultCageLayout,
+    createCageInfluenceMesh,
+    hasCageInfluenceData,
+    MAX_CAGE_CONTROLS,
+} from '../../cocos/3d/misc/cage-deform';
